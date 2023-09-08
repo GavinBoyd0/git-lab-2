c050d80396cb588490096e99dd96d38317b026a4
Gavin <gb425122@ohio.edu>
2023-09-08 18:08:19 -0400

    Enter a message here

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: gavin Boyd
+ *        Date: 9/8/2023
+ * Description: This is step 15 in the second lab for cs2400
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!" << endl;
+    return 0;
+}// main
